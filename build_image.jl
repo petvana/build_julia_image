b1cb67f58f9de7e6469adee086490c3c53b6834b
Petr Vana <petvana@centrum.cz>
2020-09-22 13:58:38 +0200

    Improve using PyPlot (now it takes only 2s + is interactive as normal)

--- build_image.jl
+++ build_image.jl
@@ -4,8 +4,14 @@ Pkg.update()
 Pkg.add("PackageCompiler")
 using PackageCompiler
 
+# There is still problem with PyPlot, see https://github.com/JuliaPy/PyPlot.jl/issues/476
+pyplot_subpackages = [:Colors, :LaTeXStrings, :PyCall, :Sockets, :Test, :VersionParsing]
+
+Pkg.add.(string(x) for x in pyplot_subpackages)
+
 create_sysimage(
-    [:JuMP, :Ipopt], # , :PyPlot 
+    [:JuMP, :Ipopt, pyplot_subpackages...], 
     sysimage_path="user_image.so", 
-    precompile_execution_file="precompiled.jl"
+    precompile_execution_file="precompiled.jl",
+    # incremental = false
 )
--- precompiled.jl
+++ precompiled.jl
@@ -6,7 +6,24 @@
 # using PyPlot
 # plt.plot(1,1,"rx")
 
+################################################################################
+module BuildIpopt
+
 using JuMP
 using Ipopt
-using JuMP
-using Ipopt
+
+model = Model(Ipopt.Optimizer)
+@variable(model, -10 <= x <= 3, start = 0.0)
+@variable(model, y, start = 13.0)
+
+@NLobjective(model, Min, (1 - x)^2 + 100 * (y - x^2)^2)
+
+optimize!(model)
+println("x = ", value(x), " y = ", value(y))
+
+# adding a (linear) constraint
+@constraint(model, x + y == 10)
+optimize!(model)
+println("x = ", value(x), " y = ", value(y))
+
+end
